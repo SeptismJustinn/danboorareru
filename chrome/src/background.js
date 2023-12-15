38ff19feeabf35d342c0b241f6268f6bccd91f0b
Tan Justinn <ohhoehoe@gmail.com>
2023-12-15 23:14:46 +0800

    Added functionality to download an auto named file from context menu
    -Injects script to find filename from pre-defined locations
    
    TODO:
    Control tags to append to name via settings

--- chrome/src/background.js
+++ chrome/src/background.js
@@ -7,6 +7,40 @@ async function initStorageCache() {
   });
 }
 
+async function getTabId() {
+  let queryOptions = { active: true, lastFocusedWindow: true };
+  // `tab` will either be a `tabs.Tab` instance or `undefined`.
+  let [tab] = await chrome.tabs.query(queryOptions);
+  return tab.id;
+}
+
+function getFilename() {
+  const characterSegment = document.querySelectorAll('.character-tag-list');
+  let charNameList;
+  for (const [idx, value] of characterSegment.entries()) {
+    if (value.tagName == 'UL') {
+      charNameList = characterSegment[idx].children;
+      break;
+    }
+  }
+  const charNames = [];
+
+  for (const listItem of charNameList) {
+    charNames.push(
+      listItem.querySelector('.search-tag').innerHTML.replace(' ', '')
+    );
+  }
+
+  const postID = document.querySelector('#post-info-id').innerHTML.split(' ');
+
+  const charNameComplex = charNames.reduce(
+    (complex, characterName) => complex + characterName,
+    ''
+  );
+
+  return `${charNameComplex}-${postID[1]}`;
+}
+
 chrome.runtime.onInstalled.addListener(() => {
   chrome.contextMenus.create({
     id: 'imagedownloader70013910',
@@ -15,11 +49,30 @@ chrome.runtime.onInstalled.addListener(() => {
   });
 });
 
+// const messageListener = chrome.runtime.onMessage.addListener((message) => {
+//   console.log(message);
+//   console.log(message.danbooru);
+//   if (message.danbooru) {
+//     storageCache.newFilename = message.filename;
+//     console.log(storageCache);
+//   }
+// });
+
 chrome.downloads.onDeterminingFilename.addListener((downloadItem, _suggest) => {
+  console.log(JSON.stringify(downloadItem));
   async function suggest() {
     // Load settings
     try {
       await initStorageCache();
+      // await messageListener;
+      await chrome.scripting
+        .executeScript({
+          target: { tabId: await getTabId() },
+          func: getFilename,
+        })
+        .then((result) => {
+          storageCache.newFilename = result[0]['result'];
+        });
     } catch (error) {
       console.log('Error loading settings');
       return false;
@@ -27,9 +80,9 @@ chrome.downloads.onDeterminingFilename.addListener((downloadItem, _suggest) => {
     // Get from settings
     const subdir = storageCache.subdirectory || 'danboorareru';
     const newFilename = storageCache.newFilename;
-
+    console.log(storageCache);
     _suggest({
-      filename: `${subdir}/${newFilename || downloadItem.filename}`,
+      filename: `${subdir}/${newFilename || downloadItem.filename}.png`,
       conflictAction: 'prompt',
     });
   }
